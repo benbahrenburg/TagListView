5433ce6e792c4ed10d89aa9d84e006545f614b28
Daniel Marulanda <d@crowdswap.com>
2019-12-05 01:07:21 -0500

    Update TagListView.swift

--- TagListView/TagListView.swift
+++ TagListView/TagListView.swift
@@ -1,431 +1,132 @@
 //
-//  TagListView.swift
-//  TagListViewDemo
+//  UserProfileViewModel.swift
+//  Crowdswap
 //
-//  Created by Dongyuan Liu on 2015-05-09.
-//  Copyright (c) 2015 Ela. All rights reserved.
+//  Created by Daniel Marulanda on 7/19/19.
+//  Copyright © 2019 Crowdswap, Inc. All rights reserved.
 //
-import UIKit
+import RxCocoa
+import RxSwift
+import RxKingfisher
+import Differentiator
+import TagListView
+struct UserProfileViewModel {
 
-@objc public protocol TagListViewDelegate {
-    @objc optional func tagPressed(_ title: String, tagView: TagView, sender: TagListView) -> Void
-    @objc optional func tagRemoveButtonPressed(_ title: String, tagView: TagView, sender: TagListView) -> Void
+  let username: Driver<String>
+  let about: Driver<String>
+  let profileImageUrl: Driver<URL?>
+  let swapsQty: Driver<String>
+  let isVerified: Driver<Bool>
+  let reputationNumber: Driver<String>
+  let userInterest: Observable<[InterestInfo]>
+
+  init (user: Observable<SwapperInfo>) {
+    self.swapsQty = user.map { $0.countSwaps ?? "0" }.asDriver(onErrorJustReturn: "")
+    self.reputationNumber = user.map { $0.reputationNumber ?? "0" }.asDriver(onErrorJustReturn: "")
+    self.about = user.map { $0.bio?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "I'm still thinking on my bio" }
+      .asDriver(onErrorJustReturn: "I'm still thinking on my bio")
+    self.username = user.map { $0.username }.asDriver(onErrorJustReturn: "")
+    self.isVerified = user.map { $0.isVerified ?? false }.asDriver(onErrorJustReturn: false)
+    self.profileImageUrl = user.map { user in
+      return URL(string: user.profilePicture ?? "https://images.crowdswap.com/octavio.png") }
+      .asDriverOnErrorJustComplete()
+    self.userInterest = user
+      .map { $0.followInterestsBySwapperId.nodes.compactMap { ($0?.interestByInterestId?.fragments.interestInfo) }  }
+  }
 }
 
-@IBDesignable
-open class TagListView: UIView {
-  
-  @IBInspectable open dynamic var wrapTagsToNextRow: Bool = true {
-      didSet {
-          rearrangeViews()
-      }
+class UserProfileView: UICollectionReusableView {
+  private(set) var disposeBag = DisposeBag()
+  var viewModel: UserProfileViewModel!
+
+  @IBOutlet weak var tagList: TagListView! {
+    didSet {
+      tagList.alignment = .left
+      tagList.wrapTagsToNextRow = false
+    }
   }
-    
-    @IBInspectable open dynamic var textColor: UIColor = .white {
-        didSet {
-            tagViews.forEach {
-                $0.textColor = textColor
-            }
-        }
+  @IBOutlet weak var swapsQtyLabel: UILabel!
+  @IBOutlet weak var praiseQtyLabel: UILabel!
+  @IBOutlet weak var isVerifiedIcon: UIImageView!
+  @IBOutlet weak var profilePic: UIImageView! {
+    didSet {
+      profilePic.makeRounded()
     }
-    
-    @IBInspectable open dynamic var selectedTextColor: UIColor = .white {
-        didSet {
-            tagViews.forEach {
-                $0.selectedTextColor = selectedTextColor
-            }
-        }
-    }
+  }
 
-    @IBInspectable open dynamic var tagLineBreakMode: NSLineBreakMode = .byTruncatingMiddle {
-        didSet {
-            tagViews.forEach {
-                $0.titleLineBreakMode = tagLineBreakMode
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var tagBackgroundColor: UIColor = UIColor.gray {
-        didSet {
-            tagViews.forEach {
-                $0.tagBackgroundColor = tagBackgroundColor
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var tagHighlightedBackgroundColor: UIColor? {
-        didSet {
-            tagViews.forEach {
-                $0.highlightedBackgroundColor = tagHighlightedBackgroundColor
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var tagSelectedBackgroundColor: UIColor? {
-        didSet {
-            tagViews.forEach {
-                $0.selectedBackgroundColor = tagSelectedBackgroundColor
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var cornerRadius: CGFloat = 0 {
-        didSet {
-            tagViews.forEach {
-                $0.cornerRadiusForTag = cornerRadius
-            }
-        }
-    }
-    @IBInspectable open dynamic var borderWidth: CGFloat = 0 {
-        didSet {
-            tagViews.forEach {
-                $0.borderWidthForTag = borderWidth
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var borderColor: UIColor? {
-        didSet {
-            tagViews.forEach {
-                $0.borderColorForTag = borderColor
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var selectedBorderColor: UIColor? {
-        didSet {
-            tagViews.forEach {
-                $0.selectedBorderColor = selectedBorderColor
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var paddingY: CGFloat = 2 {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.paddingY = paddingY
-            }
-        }
-    }
-    @IBInspectable open dynamic var paddingX: CGFloat = 5 {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.paddingX = paddingX
-            }
-        }
-    }
-    @IBInspectable open dynamic var marginY: CGFloat = 2 {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    @IBInspectable open dynamic var marginX: CGFloat = 5 {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    
-    @objc public enum Alignment: Int {
-        case left
-        case center
-        case right
-    }
-    @IBInspectable open var alignment: Alignment = .left {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    @IBInspectable open dynamic var shadowColor: UIColor = .white {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    @IBInspectable open dynamic var shadowRadius: CGFloat = 0 {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    @IBInspectable open dynamic var shadowOffset: CGSize = .zero {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    @IBInspectable open dynamic var shadowOpacity: Float = 0 {
-        didSet {
-            rearrangeViews()
-        }
-    }
-    
-    @IBInspectable open dynamic var enableRemoveButton: Bool = false {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.enableRemoveButton = enableRemoveButton
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var removeButtonIconSize: CGFloat = 12 {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.removeButtonIconSize = removeButtonIconSize
-            }
-        }
-    }
-    @IBInspectable open dynamic var removeIconLineWidth: CGFloat = 1 {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.removeIconLineWidth = removeIconLineWidth
-            }
-        }
-    }
-    
-    @IBInspectable open dynamic var removeIconLineColor: UIColor = UIColor.white.withAlphaComponent(0.54) {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.removeIconLineColor = removeIconLineColor
-            }
-        }
-    }
-    
-    @objc open dynamic var textFont: UIFont = .systemFont(ofSize: 12) {
-        didSet {
-            defer { rearrangeViews() }
-            tagViews.forEach {
-                $0.textFont = textFont
-            }
-        }
-    }
-    
-    @IBOutlet open weak var delegate: TagListViewDelegate?
-    
-    open private(set) var tagViews: [TagView] = []
-    private(set) var tagBackgroundViews: [UIView] = []
-    open private(set) var rowViews: [UIView] = []
-    private(set) var totalTagViewsWidth: CGFloat = 0
-    private(set) var tagViewHeight: CGFloat = 0
-    private(set) var rows = 0 {
-        didSet {
-            invalidateIntrinsicContentSize()
-        }
-    }
-    
-    // MARK: - Interface Builder
-    
-    open override func prepareForInterfaceBuilder() {
-        addTag("Welcome")
-        addTag("to")
-        addTag("TagListView").isSelected = true
-    }
-    
-    // MARK: - Layout
-    
-    open override func layoutSubviews() {
-        defer { rearrangeViews() }
-        super.layoutSubviews()
-    }
-    
-    private func rearrangeViews() {
-        let views = tagViews as [UIView] + tagBackgroundViews + rowViews
-        views.forEach {
-            $0.removeFromSuperview()
-        }
-        rowViews.removeAll(keepingCapacity: true)
-        
-        var currentRow = 0
-        totalTagViewsWidth = 0
-        var currentRowView: UIView!
-        var currentRowTagCount = 0
-        var currentRowWidth: CGFloat = 0
-        for (index, tagView) in tagViews.enumerated() {
-            tagView.frame.size = tagView.intrinsicContentSize
-            tagViewHeight = tagView.frame.height
-            
-          totalTagViewsWidth += tagView.frame.width
-            if currentRowTagCount == 0 ||
-                (currentRowWidth + tagView.frame.width > frame.width && wrapTagsToNextRow){
-                currentRow += 1
-                currentRowWidth = 0
-                currentRowTagCount = 0
-                currentRowView = UIView()
-                currentRowView.frame.origin.y = CGFloat(currentRow - 1) * (tagViewHeight + marginY)
-                
-                rowViews.append(currentRowView)
-                addSubview(currentRowView)
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var aboutLabel: UILabel!
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    disposeBag = DisposeBag()
 
-                tagView.frame.size.width = min(tagView.frame.size.width, frame.width)
-            }
-            
-            let tagBackgroundView = tagBackgroundViews[index]
-            tagBackgroundView.frame.origin = CGPoint(x: currentRowWidth, y: 0)
-            tagBackgroundView.frame.size = tagView.bounds.size
-            tagBackgroundView.layer.shadowColor = shadowColor.cgColor
-            tagBackgroundView.layer.shadowPath = UIBezierPath(roundedRect: tagBackgroundView.bounds, cornerRadius: cornerRadius).cgPath
-            tagBackgroundView.layer.shadowOffset = shadowOffset
-            tagBackgroundView.layer.shadowOpacity = shadowOpacity
-            tagBackgroundView.layer.shadowRadius = shadowRadius
-            tagBackgroundView.addSubview(tagView)
-            currentRowView.addSubview(tagBackgroundView)
-            
-            currentRowTagCount += 1
-            currentRowWidth += tagView.frame.width + marginX
-            
-            switch alignment {
-            case .left:
-                currentRowView.frame.origin.x = 0
-            case .center:
-                currentRowView.frame.origin.x = (frame.width - (currentRowWidth - marginX)) / 2
-            case .right:
-                currentRowView.frame.origin.x = frame.width - (currentRowWidth - marginX)
-            }
-            currentRowView.frame.size.width = currentRowWidth
-            currentRowView.frame.size.height = max(tagViewHeight, currentRowView.frame.height)
-        }
-        rows = currentRow
-        
-        invalidateIntrinsicContentSize()
-    }
+  }
+
+}
+
+extension UserProfileView: BindableType {
+  func bindViewModel() {
+    disposeBag.insert(
+      //Information Binding
+      viewModel.isVerified.map{ !$0 }.drive(isVerifiedIcon.rx.isHidden),
+      viewModel.swapsQty.drive(swapsQtyLabel.rx.text),
+      viewModel.reputationNumber.drive(praiseQtyLabel.rx.text),
+      viewModel.username.drive(nameLabel.rx.text),
+      viewModel.about.drive(aboutLabel.rx.text),
+      viewModel.profileImageUrl.drive(onNext: { [unowned self] url in
+        self.profilePic.kf.setImage(with: url, options: [.transition(.fade(0.2))])
+      })
+    )
     
-    // MARK: - Manage tags
-    
-    override open var intrinsicContentSize: CGSize {
-        var height = CGFloat(rows) * (tagViewHeight + marginY)
-        if rows > 0 {
-            height -= marginY
-        }
-          var width = frame.width
-      if !wrapTagsToNextRow {
-          width = (CGFloat(tagViews.count - 1) * marginX) + totalTagViewsWidth
+    viewModel.userInterest
+    .do(onNext: { [unowned self] interest in
+      let list: [TagView] = interest
+        .filter { $0.groupingSet == nil }
+        .compactMap { interest in
+          let view = TagView(title: interest.name.localized)
+          view.gradientSelectedColor = true
+          view.selectedBackgroundColor = UIColor(hex: "#\(interest.hexColor ?? "#000000")", alpha: 1.0)
+          view.selectedTextColor = .white
+          view.selectedBorderColor = .clear
+          view.borderWidthForTag = 1.0
+          view.cornerRadiusForTag = 17.0
+          view.paddingX = 15
+          view.paddingY = 10
+          view.borderColorForTag = UIColor(hex: "#\(interest.hexColor ?? "#000000")", alpha: 0.16)
+          view.textColor = .white
+          view.textFont = UIFont(name: "Avenir-Heavy", size: 17.0)!
+          view.tagBackgroundColor = UIColor(hex: "#\(interest.hexColor ?? "#000000")", alpha: 1.0) ?? .white
+          view.tag = interest.id
+          return view
+      } 
+
+      let sets: [TagView] = interest.removingDuplicates()
+        .filter { $0.groupingSet != nil }
+        .compactMap { interest in
+          let view = TagView(title: interest.groupingSet!.localized)
+          view.gradientSelectedColor = true
+          view.selectedBackgroundColor = UIColor(hex: "#\(interest.hexColor ?? "#000000")", alpha: 1.0)
+          view.selectedTextColor = .white
+          view.selectedBorderColor = .clear
+          view.borderWidthForTag = 1.0
+          view.cornerRadiusForTag = 17.0
+          view.paddingX = 15
+          view.paddingY = 10
+          view.borderColorForTag = UIColor(hex: "#\(interest.hexColor ?? "#000000")", alpha: 0.16)
+          view.textColor = .white
+          view.textFont = UIFont(name: "Avenir-Heavy", size: 17.0)!
+          view.tagBackgroundColor = UIColor(hex: "#\(interest.hexColor ?? "#000000")", alpha: 1.0) ?? .white
+          view.isUserInteractionEnabled = false
+          return view
       }
-      return CGSize(width: width, height: height)
-    }
-    
-    private func createNewTagView(_ title: String) -> TagView {
-        let tagView = TagView(title: title)
-        
-        tagView.textColor = textColor
-        tagView.selectedTextColor = selectedTextColor
-        tagView.tagBackgroundColor = tagBackgroundColor
-        tagView.highlightedBackgroundColor = tagHighlightedBackgroundColor
-        tagView.selectedBackgroundColor = tagSelectedBackgroundColor
-        tagView.titleLineBreakMode = tagLineBreakMode
-        tagView.cornerRadiusForTag = cornerRadius
-        tagView.borderWidthForTag = borderWidth
-        tagView.borderColorForTag = borderColor
-        tagView.selectedBorderColor = selectedBorderColor
-        tagView.paddingX = paddingX
-        tagView.paddingY = paddingY
-        tagView.textFont = textFont
-        tagView.removeIconLineWidth = removeIconLineWidth
-        tagView.removeButtonIconSize = removeButtonIconSize
-        tagView.enableRemoveButton = enableRemoveButton
-        tagView.removeIconLineColor = removeIconLineColor
-        tagView.addTarget(self, action: #selector(tagPressed(_:)), for: .touchUpInside)
-        tagView.removeButton.addTarget(self, action: #selector(removeButtonPressed(_:)), for: .touchUpInside)
-        
-        // On long press, deselect all tags except this one
-        tagView.onLongPress = { [unowned self] this in
-            self.tagViews.forEach {
-                $0.isSelected = $0 == this
-            }
-        }
-        
-        return tagView
-    }
 
-    @discardableResult
-    open func addTag(_ title: String) -> TagView {
-        defer { rearrangeViews() }
-        return addTagView(createNewTagView(title))
-    }
-    
-    @discardableResult
-    open func addTags(_ titles: [String]) -> [TagView] {
-        return addTagViews(titles.map(createNewTagView))
-    }
-    
-    @discardableResult
-    open func addTagView(_ tagView: TagView) -> TagView {
-        defer { rearrangeViews() }
-        tagView.addTarget(self, action: #selector(tagPressed(_:)), for: .touchUpInside)
-        tagViews.append(tagView)
-        tagBackgroundViews.append(UIView(frame: tagView.bounds))
-        
-        return tagView
-    }
-    
-    @discardableResult
-    open func addTagViews(_ tagViews: [TagView]) -> [TagView] {
-        tagViews.forEach {
-            addTagView($0)
-        }
-        return tagViews
-    }
-
-    @discardableResult
-    open func insertTag(_ title: String, at index: Int) -> TagView {
-        return insertTagView(createNewTagView(title), at: index)
-    }
-    
-
-    @discardableResult
-    open func insertTagView(_ tagView: TagView, at index: Int) -> TagView {
-        defer { rearrangeViews() }
-        tagView.addTarget(self, action: #selector(tagPressed(_:)), for: .touchUpInside)
-        tagViews.insert(tagView, at: index)
-        tagBackgroundViews.insert(UIView(frame: tagView.bounds), at: index)
-        
-        return tagView
-    }
-    
-    open func setTitle(_ title: String, at index: Int) {
-        tagViews[index].titleLabel?.text = title
-    }
-    
-    open func removeTag(_ title: String) {
-        tagViews.reversed().filter({ $0.currentTitle == title }).forEach(removeTagView)
-    }
-    
-    open func removeTagView(_ tagView: TagView) {
-        defer { rearrangeViews() }
-        
-        tagView.removeFromSuperview()
-        if let index = tagViews.firstIndex(of: tagView) {
-            tagViews.remove(at: index)
-            tagBackgroundViews.remove(at: index)
-        }
-    }
-    
-    open func removeAllTags() {
-        defer {
-            tagViews = []
-            tagBackgroundViews = []
-            rearrangeViews()
-        }
-        
-        let views: [UIView] = tagViews + tagBackgroundViews
-        views.forEach { $0.removeFromSuperview() }
-    }
-
-    open func selectedTags() -> [TagView] {
-        return tagViews.filter { $0.isSelected }
-    }
-    
-    // MARK: - Events
-    
-    @objc func tagPressed(_ sender: TagView!) {
-        sender.onTap?(sender)
-        delegate?.tagPressed?(sender.currentTitle ?? "", tagView: sender, sender: self)
-    }
-    
-    @objc func removeButtonPressed(_ closeButton: CloseButton!) {
-        if let tagView = closeButton.tagView {
-            delegate?.tagRemoveButtonPressed?(tagView.currentTitle ?? "", tagView: tagView, sender: self)
-        }
-    }
+      let views = (sets + list)
+      self.tagList.addTagViews(views)
+    })
+      .subscribe(onNext: { _ in
+        self.tagList.tagViews.forEach { $0.isSelected = true }
+      })
+    .disposed(by: disposeBag)
+  }
 }
